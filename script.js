9883b7a05338a1b06518a2ea7efee3c48b2c0cd8
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2022-07-29 15:10:00 +0300

    Add lmb, mmb, rmb detection and paint color instead of darken
    Left click paints a single rgba color instead of increasing the alpha of
    black color. Color will be selectable in the future.

--- script.js
+++ script.js
@@ -2,15 +2,41 @@ const container = document.querySelector('.container');
 const resizeButton = document.querySelector('#resize');
 resizeButton.addEventListener('click', promptForSize);
 let size = 16;
-let isMouseDown = false;
+let lmbDown = false;
+let rmbDown = false;
+let mmbDown = false;
 
 createGrid(size);
 
 onmousedown = (e) => {
-    isMouseDown = true;
+    switch (e.button) {
+        case 0:
+            lmbDown = true;
+            break;
+        case 1:
+            mmbDown = true;
+            break;
+        case 2:
+            rmbDown = true;
+            break;
+        default:
+            break;
+    }
 }
 onmouseup = (e) => {
-    isMouseDown = false;
+    switch (e.button) {
+        case 0:
+            lmbDown = false;
+            break;
+        case 1:
+            mmbDown = false;
+            break;
+        case 2:
+            rmbDown = false;
+            break;
+        default:
+            break;
+    }
 }
 
 function createGrid(sideLength) {
@@ -23,17 +49,26 @@ function createGrid(sideLength) {
             cell.classList.add('cell');
             cell.setAttribute('onmousedown', 'return false');
             cell.addEventListener('mouseenter', (e) => {
-                if (isMouseDown) {
-                    darken(cell);
-                }
+                handleMouseButtons(cell);
             });
             cell.addEventListener('mousedown', (e) => {
-                darken(cell);
+                handleMouseButtons(cell);
+            });
+            cell.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
             });
 
             row.appendChild(cell);
         }
         container.appendChild(row);
+    }
+}
+
+function handleMouseButtons(cell) {
+    if (lmbDown) {
+        paintCell(cell);
+    } else if (rmbDown) {
+        clearCell(cell);
     }
 }
 
@@ -45,14 +80,19 @@ function promptForSize() {
     }
 }
 
+function paintCell(element, color = 'rgba(0, 0, 0, 1)') {
+    element.style.backgroundColor = color;
+}
+
 function darken(element, strength = 0.334) {
-    // element.style.backgroundColor = 'black';
     let color = getComputedStyle(element).backgroundColor;
     const lastComma = color.lastIndexOf(',');
     let alpha = parseFloat(color.slice(lastComma + 1, color.lastIndexOf(')')));
     alpha += strength;
-    console.log(alpha);
     color = color.slice(0, lastComma) + `, ${alpha})`;
     element.style.backgroundColor = color;
-    // console.log(getComputedStyle(element).backgroundColor);
+}
+
+function clearCell(element) {
+    element.style.backgroundColor = 'rgba(255, 255, 255, 1)';
 }
